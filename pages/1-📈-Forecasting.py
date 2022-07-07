4682e02ebf75ac84d4612ca6acce9323b86d7c60
sam-the-programmer <user@example.com>
2022-07-07 20:09:47 +0100

    add files

--- pages/1-📈-Forecasting.py
+++ pages/1-📈-Forecasting.py
@@ -6,7 +6,7 @@ st.set_page_config(
     layout="wide",
 )
 
-import yfinance as yf
+from stocksearchpro.data import get as get_data
 from stocksearchpro.data import tickers
 from stocksearchpro.helpers import ui
 
@@ -14,4 +14,21 @@ ui.ui()
 
 st.title("Forecasting")
 st.write("Forecast stocks with machine learning.")
-st.selectbox("Choose Stock", tickers)
+
+stockname = st.selectbox("Choose Stock", tickers)
+stockinfo = {
+    "name": stockname.split("-")[1].strip(),
+    "symbol": stockname.split("-")[0].strip(),
+}
+
+cols = st.columns([3, 1])
+with cols[0]:
+    period = st.select_slider(
+        "Select Time Period", ["1d", "5d", "1mo", "3mo", "1y", "5y", "10y", "ytd"], "1d"
+    )
+
+with cols[1]:
+    types = ["Open", "Close", "High", "Low", "Adj Close", "Volume"]
+    boxes = [False for _ in range(len(types))]
+    for i, type_ in enumerate(types):
+        boxes[i] = st.checkbox(type_)
--- stocksearchpro/data/__init__.py
+++ stocksearchpro/data/__init__.py
@@ -1,4 +1,4 @@
 import pathlib
-
+from stocksearchpro.data.yahoo import get
 
 tickers = pathlib.Path("stocksearchpro/data/tickers_names.txt").read_text().split("\n")
